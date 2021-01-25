85601d13062b9e871cdd1a9e89f729024bfb0315
Hassan Mazyad <hassan.mazyad@outlook.com>
2021-01-26 00:08:59 +0100

    Video 1

--- assignment-app/src/app/app.module.ts
+++ assignment-app/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { AssignmentsComponent } from './assignments/assignments.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ assignment-app/src/app/assignments/assignments.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css']
+})
+export class AssignmentsComponent implements OnInit {
+
+  titre = "Mon application sur les Assignments !"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
