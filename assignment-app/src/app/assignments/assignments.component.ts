8e89baa99fe7816fe60f2d4a76e013f8d2e9db31
Hassan Mazyad <hassan.mazyad@outlook.com>
2021-01-26 22:20:07 +0100

    fin video 1

--- assignment-app/src/app/assignments/assignments.component.ts
+++ assignment-app/src/app/assignments/assignments.component.ts
@@ -9,6 +9,25 @@ export class AssignmentsComponent implements OnInit {
 
   titre = "Mon application sur les Assignments !"
 
+  assignments = [
+    {
+      nom:"TP1 Web component",
+      dataDeRendu:'2020-11-17',
+      rendu:true
+    },
+    {
+      nom:"TP2 Web Angular",
+      dataDeRendu:'2020-12-13',
+      rendu:false
+    },
+    {
+      nom:"Mini projet Angular",
+      dataDeRendu:'2021-01-07',
+      rendu:false
+    }
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
